c058bbe18f3a6bb3dd275e287fe635233f773e6e
Katerina Komnatnaya <comina.ru@gmail.com>
2017-08-14 20:47:03 +0300

    colors

--- js/stat.js
+++ js/stat.js
@@ -1,26 +1,24 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 430, 280);
-  ctx.fillStyle = 'white';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+  var heightCloud = 270;
+  var widthCloud = 420;
+  var beginCloudX = 100;
+  var beginCloudY = 10;
+  var shiftShadowXY = 10;
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили', 155, 20);
-  ctx.fillText('Список результатов:', 155, 40);
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(beginCloudX + shiftShadowXY, beginCloudY + shiftShadowXY, widthCloud + shiftShadowXY, heightCloud + shiftShadowXY);
+  ctx.fillStyle = 'white';
+  ctx.strokeRect(beginCloudX, beginCloudY, widthCloud, heightCloud);
+  ctx.fillRect(beginCloudX, beginCloudY, widthCloud, heightCloud);
 
   var max = -1;
-  // var maxIndex = -1;
 
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
-      // maxIndex = i;
     }
   }
 
@@ -29,11 +27,27 @@ window.renderStatistics = function (ctx, names, times) {
 
   var barWidth = 40;
   var stepWidth = 50;
-  var initialX = 155;
+  // var initialX = 155;
+  var initialX = beginCloudX + (widthCloud - barWidth * names.length - stepWidth * (names.length - 1)) / 2;
   var initialY = 240;
 
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили', initialX, 20);
+  ctx.fillText('Список результатов:', initialX, 40);
+
   for (i = 0; i < times.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      var transparency = Math.random() + 0.1;
+      // var transparency = i * 0.1 + 0.5;
+      ctx.fillStyle = 'rgba(0, 0, 255,' + transparency + ')';
+    }
     ctx.fillRect(initialX + (barWidth + stepWidth) * i, initialY, barWidth, -1 * times[i] * step);
+
+    ctx.fillStyle = '#000';
     ctx.fillText(names[i], initialX + (barWidth + stepWidth) * i, initialY + 10);
     ctx.fillText(Math.floor(times[i]), initialX + (barWidth + stepWidth) * i, initialY - times[i] * step - 20);
   }
